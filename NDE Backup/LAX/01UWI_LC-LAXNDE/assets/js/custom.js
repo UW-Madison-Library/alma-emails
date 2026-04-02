01bd98d5ae840393b42ba17496ec5d54ffd2ed34
UWkatiesanders <144488603+UWkatiesanders@users.noreply.github.com>
2026-04-02 15:25:09 -0500

    Uploaded the La Crosse package

---
+++ NDE Backup/LAX/01UWI_LC-LAXNDE/assets/js/custom.js
@@ -0,0 +1,9 @@
+/----------Code for LibraryH3lp chat widget-----------/
+var needsJs = document.createElement('div');
+needsJs.setAttribute('class', 'needs-js');
+document.body.appendChild(needsJs);
+
+var s = document.createElement('script');
+s.id = 'localScript';
+s.src = 'https://libraryh3lp.com/js/libraryh3lp.js?34998';
+document.body.appendChild(s);
